02695887341c8ebbee0c6df1efd110143b5506be
SifatKhan30 <sifatkhan9330@gmail.com>
2024-01-10 13:24:26 +0600

    git initiated

---
+++ app/Http/Controllers/serviceController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+class serviceController extends Controller
+{
+    
+    public function index()
+    {
+        $list = Service::all();
+        return response()->json(['message'=>'data fetched successfully','data'=>$list]);
+        // dd($list);
+    }
+
+   
+    public function create()
+    {
+        //
+    }
+
+   
+    public function store(Request $request)
+    {
+       $validator= Validator::make($request->all(), [
+            'name'=>'required',
+            'category'=>'required',
+            'price'=>'required|numeric',
+            
+        ]);
+        if ($validator->fails()) {
+            return response()->json(["code"=>422,'errors' => $validator->errors(),], 422);
+        }
+        $data = $request->all();
+        Service::create($data);
+            return response()->json(["code"=>201, "message"=>"insert Successfull", 'data'=>$data], 201);
+    }
+
+   
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+  
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_01_09_063747_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->char('name',100);
+            $table->char('category',100);
+            $table->decimal('price',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
